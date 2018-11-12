f0338ef17cf6f94548a2b193718ca01d4c821f40
chenhengwei <onlylimit@gmail.com>
2018-11-12 11:56:58 +0800

    Initial Commit

---
+++ MakeYouOwnBadge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MakeYouOwnBadge
+//
+//  Created by CHEN WESLEY on 2018/11/12.
+//  Copyright © 2018 CHEN WESLEY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
